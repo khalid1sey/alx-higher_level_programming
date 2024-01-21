dc9e55a365f0318b122bffb134eaf5ac6475c292
khalid1sey <lilahseyfu@gmail.com>
2024-01-21 18:16:22 +0300

    1.done t2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (my_list)
+
+    return (my_list.remove(idx))
