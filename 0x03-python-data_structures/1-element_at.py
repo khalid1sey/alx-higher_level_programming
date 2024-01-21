bc53f4b374c82cf1598329da0845c04ec017b353
khalid1sey <lilahseyfu@gmail.com>
2024-01-21 17:55:01 +0300

    3.done t1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,12 @@
 def element_at(my_list, idx):
     """A function that retrives an element of a list"""
 
-    if idx < 0 and idx > len:
-        return
-    else:
+    if idx < 0 :
+        return(None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
         return my_list[idx]
+
+    return(my_list[idx])
